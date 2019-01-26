6b7a87e4c7d623c89d11d762e179766b3c666bb5
dallonf <dallon.feldner@gmail.com>
2019-01-26 17:21:56 -0600

    Added music!

--- Assets/Scripts/Flow/LevelTransition.cs
+++ Assets/Scripts/Flow/LevelTransition.cs
@@ -14,6 +14,7 @@ public class LevelTransition : MonoBehaviour
   }
 
   public Image FadeToBlackImage;
+  public AudioSource Music;
   public bool ShouldFadeIn = true;
   public float FadeDuration = 1;
 
@@ -24,6 +25,11 @@ public class LevelTransition : MonoBehaviour
       PauseState.Instance.CurrentState = PauseState.State.TRANSITION;
       FadeToBlackImage.gameObject.SetActive(true);
       FadeToBlackImage.color = Color.black;
+      if (Music)
+      {
+        Music.volume = 0;
+        DOTween.To(() => Music.volume, x => Music.volume = x, 1, FadeDuration);
+      }
       DOTween
         .To(
           () => FadeToBlackImage.color,
@@ -48,6 +54,10 @@ public class LevelTransition : MonoBehaviour
     PauseState.Instance.CurrentState = PauseState.State.TRANSITION;
     FadeToBlackImage.gameObject.SetActive(true);
     FadeToBlackImage.color = new Color(0, 0, 0, 0);
+    if (Music)
+    {
+      DOTween.To(() => Music.volume, x => Music.volume = x, 0, FadeDuration);
+    }
     DOTween
       .To(
         () => FadeToBlackImage.color,
