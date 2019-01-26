c86b5ce28a45696adeb04ad7a36fcfe572035d46
dallonf <dallon.feldner@gmail.com>
2019-01-26 13:55:53 -0600

    Refactored text box system to allow character names

--- Assets/Scripts/Flow/DialogSystem.cs
+++ Assets/Scripts/Flow/DialogSystem.cs
@@ -18,18 +18,18 @@ public class DialogSystem : MonoBehaviour
     TextBox.gameObject.SetActive(false);
   }
 
-  public IEnumerator ShowText(string text)
+  public IEnumerator ShowText(DialogMessage message)
   {
     var prevState = PauseState.Instance.CurrentState;
     TextBox.gameObject.SetActive(true);
     PauseState.Instance.CurrentState = PauseState.State.DIALOG;
-    yield return StartCoroutine(TextBox.ShowText(text));
+    yield return StartCoroutine(TextBox.ShowText(message));
     TextBox.gameObject.SetActive(false);
     PauseState.Instance.CurrentState = prevState;
   }
 
-  public void StartShowText(string text)
+  public void StartShowText(DialogMessage message)
   {
-    StartCoroutine(ShowText(text));
+    StartCoroutine(ShowText(message));
   }
 }
---
+++ Assets/Scripts/UI/DialogMessage.cs
@@ -0,0 +1,11 @@
+public struct DialogMessage
+{
+  public string CharacterName;
+  public string Text;
+
+  public DialogMessage(string characterName, string text)
+  {
+    CharacterName = characterName;
+    Text = text;
+  }
+}
