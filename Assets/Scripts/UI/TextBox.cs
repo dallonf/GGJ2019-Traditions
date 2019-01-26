5acfe022ead16a027800a23a9f8b6ff809d39780
dallonf <dallon.feldner@gmail.com>
2019-01-26 11:41:38 -0600

    Text incoming animation

--- Assets/Scripts/UI/TextBox.cs
+++ Assets/Scripts/UI/TextBox.cs
@@ -1,16 +1,28 @@
 using System.Collections;
+using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
 
   public TextMeshProUGUI textMesh;
+  public Image nextIcon;
+  public float CharacterDelay = 0.05f;
 
   public IEnumerator ShowText(string text)
   {
+    nextIcon.enabled = false;
+    for (int i = 0; i < text.Length; i++)
+    {
+      textMesh.text = text.Substring(0, i);
+      yield return new WaitForSecondsRealtime(CharacterDelay);
+    }
     textMesh.text = text;
-    while (!Input.GetButtonDown("Interact")) {
+    nextIcon.enabled = true;
+    while (!Input.anyKeyDown)
+    {
       // wait for user to close text box
       yield return null;
     }
