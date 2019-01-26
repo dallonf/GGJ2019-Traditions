2a421a6b4988e66d6046bba1fee7497fd86cc2e4
dallonf <dallon.feldner@gmail.com>
2019-01-26 08:33:19 -0600

    Basic player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody2D;
+
+    public float Speed;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(h, v) * Speed;
+    }
+}
