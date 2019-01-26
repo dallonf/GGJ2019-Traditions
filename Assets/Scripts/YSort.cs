b1b35796601815445c917557e6c570a2a0e0375d
dallonf <dallon.feldner@gmail.com>
2019-01-26 09:29:19 -0600

    Simple camera

--- Assets/Scripts/YSort.cs
+++ Assets/Scripts/YSort.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class YSort : MonoBehaviour
 {
+
+  private SpriteRenderer spriteRenderer;
+
+  void Awake()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
   void Update()
   {
     transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
