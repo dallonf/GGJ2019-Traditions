ee832cdcd8ad700633ad7240dca420db5e03cdb6
dallonf <dallon.feldner@gmail.com>
2019-01-27 09:58:00 -0600

    Added pickup sound for raspberries

--- Assets/Scenes/GameLevels/raspberries/RaspberriesEvent.cs
+++ Assets/Scenes/GameLevels/raspberries/RaspberriesEvent.cs
@@ -5,6 +5,7 @@ public class RaspberriesEvent : MonoBehaviour
 {
     public ProgressFlag MomTalkFlag;
     public ProgressFlag ChristmasLightFlag;
+    public AudioSource PickupSound;
 
     public void TriggerEvent()
     {
@@ -15,23 +16,25 @@ public class RaspberriesEvent : MonoBehaviour
     public IEnumerator EventCoroutine()
     {
         if (Progress.Instance.GetProgressFlagState(MomTalkFlag) >= 1 &&
-          Progress.Instance.GetProgressFlagState(ChristmasLightFlag) < 6)
+            Progress.Instance.GetProgressFlagState(ChristmasLightFlag) < 6)
         {
             Progress.Instance.IncrementProgressFlag(ChristmasLightFlag);
             if (Progress.Instance.GetProgressFlagState(ChristmasLightFlag) == 6)
             {
                 yield return StartCoroutine(DialogSystem.Instance.ShowText(
-        new DialogMessage
-        {
-            Text = "* You've found enough raspberries for the pie."
-        }
-      ));
+                    new DialogMessage
+                    {
+                        Text = "* You've found enough raspberries for the pie."
+                    }
+                ));
             }
 
         }
-        if (Progress.Instance.GetProgressFlagState(MomTalkFlag) >= 1) {
+        if (Progress.Instance.GetProgressFlagState(MomTalkFlag) >= 1)
+        {
+            PickupSound.Play();
             Destroy(transform.parent.gameObject);
         }
-        
+
     }
 }
